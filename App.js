0b821ed0d814014f14abbcc7c2796745d3503c08
MUSTAFA FEHMİ ALTINBAŞ <mustafaaltinbas@std.sehir.edu.tr>
2021-07-01 11:58:46 +0300

    First RN Project

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './scr/screens/SearchScreen';
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "BusinessSearch"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
